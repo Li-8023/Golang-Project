7898de7bf1d5451a2e234ad783b5c25d0c2360d4
Li He <80284104+Li-8023@users.noreply.github.com>
2024-04-08 15:27:38 +0800

    chat page

--- models/message.go
+++ models/message.go
@@ -18,8 +18,8 @@ type Message struct {
 	gorm.Model
 	FromId   int64  //发送者
 	TargetId int64  //消息接收者
-	Type     int    //发送类型 （群聊，私聊）
-	Media    int    //消息类型 （文字，图片，音频）
+	Type     int    //发送类型 （1私聊，2群聊，3广播）
+	Media    int    //消息类型 （1文字，2表情包，3图片，4音频）
 	Content  string //消息内容
 	Pic      string
 	Url      string
--- service/index.go
+++ service/index.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"fmt"
+	"ginchat/models"
 	"html/template"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,11 +22,36 @@ func GetIndex(c *gin.Context) {
 	ind.Execute(c.Writer, "index")
 }
 
-
 func ToRegister(c *gin.Context) {
 	ind, err := template.ParseFiles("views/user/register.html")
 	if err != nil {
 		panic(err)
 	}
 	ind.Execute(c.Writer, "register")
+}
+
+func ToChat(c *gin.Context) {
+	ind, err := template.ParseFiles("views/chat/index.html",
+	"views/chat/head.html",
+	"views/chat/tabmenu.html",
+	"views/chat/concat.html",
+	"views/chat/group.html",
+	"views/chat/profile.html",
+	"views/chat/main.html",
+	"views/chat/createcom.html",
+	"views/chat/userinfo.html",
+	"views/chat/foot.html",
+)
+	if err != nil {
+		panic(err)
+	}
+
+	userId, _ := strconv.Atoi(c.Query("userId"))
+	token := c.Query("token")
+	user := models.UserBasic{}
+	user.ID = uint(userId)
+	user.Identity = token
+	
+	fmt.Println("ToChat>>>>>", user)
+	ind.Execute(c.Writer, user)
 }
